70cb372d70d80731043e9046912dd67129c18486
Your Name <catencezincate@gmail.com>
2017-10-14 00:25:33 -0700

    updated

---
+++ Fancier/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fancier
+{
+    public abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            cate myString = new cate();
+            myString.cook();
+        }
+    }
+    class cynthia
+    {
+        public int MyProperty { get;}
+        public void cook()
+        {
+            Console.Write("Print this");
+        }
+    }
+    class cate:cynthia
+    {
+        public cate():base()
+        {
+           //cook();       
+        }
+    }
+}
